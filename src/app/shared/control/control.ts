ccece163cd3c93467cca53c56d8ac09b8c7c69eb
lucaslopescunha <lucaslopescunha@gmail.com>
2026-02-22 16:22:26 -0300

    Interacting with Host elements using @HostBinding and @HostListener.

--- src/app/shared/control/control.ts
+++ src/app/shared/control/control.ts
@@ -1,25 +1,21 @@
-import { Component, Host, HostBinding, input, ViewEncapsulation } from '@angular/core';
+import { Component, Host, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   imports: [],
   templateUrl: './control.html',
   styleUrl: './control.css',
-  encapsulation: ViewEncapsulation.None,
-  host: {
-    class: 'control', /** outra maneira de colocar o control. 
-    Interacting with host elements from inside components */
-    '(click)': 'onClick()',/** When you click in a ngContent item
-      ANGULAR TEAM RECOMENDS THIS APPROACH!!!
-    */ 
-  }
+  encapsulation: ViewEncapsulation.None
 })
 export class ControlComponent {
-  // @HostBinding('class') className = 'control'; Opção ao host property
-  label = input.required<string>();
-
-  onClick() {
-    /** ANGULAR TEAM RECOMENDS THIS APPROACH!! */
+  @HostBinding('class') className = 'control';// Opção ao host property
+  @HostListener('click') onClick() {
     console.log('Clicked!');
   }
+  label = input.required<string>();
+
+  //onClick() {
+    /** ANGULAR TEAM RECOMENDS THIS APPROACH!! */
+    //console.log('Clicked!');
+  //}
 }
