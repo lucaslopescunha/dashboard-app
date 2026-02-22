a2c8606b66070597dc31b5764b1e0da349c0a946
lucaslopescunha <lucaslopescunha@gmail.com>
2026-02-21 22:19:11 -0300

    Reference button

--- src/app/dashboard/tickets/new-ticket/new-ticket.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../shared/button/button';
 
 @Component({
   selector: 'app-new-ticket',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './new-ticket.html',
   styleUrl: './new-ticket.css',
 })
