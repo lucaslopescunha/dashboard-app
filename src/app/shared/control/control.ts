7dff78c0ce32df983c5d6a06178cb86253e0da42
lucaslopescunha <lucaslopescunha@gmail.com>
2026-02-22 16:23:55 -0300

    Host binding on click in a declarative way. Using angular standard.

--- src/app/shared/control/control.ts
+++ src/app/shared/control/control.ts
@@ -1,21 +1,25 @@
-import { Component, Host, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
+import { Component, Host, HostBinding, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   imports: [],
   templateUrl: './control.html',
   styleUrl: './control.css',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control', /** outra maneira de colocar o control. 
+    Interacting with host elements from inside components */
+    '(click)': 'onClick()',/** When you click in a ngContent item
+      ANGULAR TEAM RECOMENDS THIS APPROACH!!!
+    */ 
+  }
 })
 export class ControlComponent {
-  @HostBinding('class') className = 'control';// Opção ao host property
-  @HostListener('click') onClick() {
-    console.log('Clicked!');
-  }
+  // @HostBinding('class') className = 'control'; Opção ao host property
   label = input.required<string>();
 
-  //onClick() {
+  onClick() {
     /** ANGULAR TEAM RECOMENDS THIS APPROACH!! */
-    //console.log('Clicked!');
-  //}
+    console.log('Clicked!');
+  }
 }
