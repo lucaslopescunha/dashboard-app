19a70d41caffac2826ddff9b4018abc20c65009a
lucaslopescunha <lucaslopescunha@gmail.com>
2026-02-23 14:36:53 -0300

    143. A close look at template for loops.

--- src/app/dashboard/tickets/new-ticket/new-ticket.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.ts
@@ -1,7 +1,8 @@
-import { AfterViewInit, Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, output, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button';
 import { ControlComponent } from "../../../shared/control/control";
 import { FormsModule } from '@angular/forms';
+import { tick } from '@angular/core/testing';
 
 @Component({
   selector: 'app-new-ticket',
@@ -13,16 +14,14 @@ import { FormsModule } from '@angular/forms';
 export class NewTicketComponent implements AfterViewInit {
   //@ViewChild('form') form?: ElementRef<HTMLFormElement>;
   private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+  add = output<{ title: string; text: string }>();
 
   ngAfterViewInit(): void {
     console.log('After View Init', this.form().nativeElement);
-    
   }
 
   onSubmit(title: string, ticketText: string) {
-    console.log("title is", title);
-    console.log("ticket input", ticketText);
-    //console.dir(form);
+    this.add.emit({ title: title, text: ticketText });
     this.form().nativeElement.reset(); // reseta os valores do form.
     //console.dir(titleElement); // prints the object structure
   }
--- src/app/dashboard/tickets/tickets.ts
+++ src/app/dashboard/tickets/tickets.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { NewTicketComponent } from "./new-ticket/new-ticket";
 import { Ticket } from './ticket.model';
+import { randomUUID } from 'node:crypto';
+import { TicketComponent } from "./ticket/ticket";
 
 @Component({
   selector: 'app-tickets',
   templateUrl: './tickets.html',
   styleUrl: './tickets.css',
-  imports: [NewTicketComponent],
+  imports: [NewTicketComponent, TicketComponent],
 })
 export class TicketsComponent {
   tickets: Ticket[] = [];
+
+  onAdd(ticketData: {title: string; text: string}) {
+    const ticket: Ticket = {
+      title: ticketData.title,
+      request: ticketData.text,
+      id: (Math.floor(Math.random() * (100000 - 1 + 1)) + 1) + '' ,
+      status: 'open'
+    }
+    this.tickets.unshift(ticket);
+  }
 }
