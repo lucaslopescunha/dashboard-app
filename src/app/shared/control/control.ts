23b00aea38bcccadb3bb243ab6e44608ffe51488
lucaslopescunha <lucaslopescunha@gmail.com>
2026-02-23 11:06:35 -0300

    Recuperando inputs do ngCotent usando signals(melhor approach).  private control = contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input');

--- src/app/shared/control/control.ts
+++ src/app/shared/control/control.ts
@@ -1,4 +1,4 @@
-import { Component, ContentChild, ElementRef, inject, input, ViewEncapsulation } from '@angular/core';
+import { Component, contentChild, ContentChild, ElementRef, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -11,24 +11,25 @@ import { Component, ContentChild, ElementRef, inject, input, ViewEncapsulation }
     Interacting with host elements from inside components */
     '(click)': 'onClick()',/** When you click in a ngContent item
       ANGULAR TEAM RECOMENDS THIS APPROACH!!! Event Listening.
-    */ 
+    */
   }
 })
 export class ControlComponent {
   // @HostBinding('class') className = 'control'; Opção ao host property
   label = input.required<string>();
-  private el = inject(ElementRef); 
+  private el = inject(ElementRef);
   /** ElementRef Is a class defined by angular framework which defines a 
     reference to some element thats rendered to the page. Can refer to any element
     on the page. Angular will give you access to the host element of that component.
     app-control in case. */
-    @ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>;
+  //@ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>;
+  private control = contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input');
 
   onClick() {
     /** ANGULAR TEAM RECOMENDS THIS APPROACH!! */
     console.log('Clicked!');
     console.log(this.el);
-    console.log(this.control);
+    console.log(this.control());
   }
 
 }
