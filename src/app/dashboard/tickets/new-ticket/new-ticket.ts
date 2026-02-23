c16b1788a9e73ef6ea9b2d251b03fac31e49fd5a
lucaslopescunha <lucaslopescunha@gmail.com>
2026-02-23 10:07:07 -0300

    New way of getting hold of an element that is stored in the template.

--- src/app/dashboard/tickets/new-ticket/new-ticket.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button';
 import { ControlComponent } from "../../../shared/control/control";
 import { FormsModule } from '@angular/forms';
@@ -11,11 +11,13 @@ import { FormsModule } from '@angular/forms';
 
 })
 export class NewTicketComponent {
- onSubmit(title: string, ticketText: string, form: HTMLFormElement) {
-  console.log("title is", title);
-  console.log("ticket input", ticketText);
-  //console.dir(form);
-  form.reset(); // reseta os valores do form.
-  //console.dir(titleElement); // prints the object structure
- }
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+
+  onSubmit(title: string, ticketText: string) {
+    console.log("title is", title);
+    console.log("ticket input", ticketText);
+    //console.dir(form);
+    this.form?.nativeElement.reset(); // reseta os valores do form.
+    //console.dir(titleElement); // prints the object structure
+  }
 }
